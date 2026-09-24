1e1fb0f027b1122bfc5280db630f53068b000630
Dzornoboa <37811509+dzornoboa@users.noreply.github.com>
2026-09-24 17:46:08 +0000

    Remove obsolete bundled sprite source doorSprite.js

--- src/assets/doorSprite.js
+++
@@ -1,8 +0,0 @@
-import p0 from './doorSpritePart0'
-import p1 from './doorSpritePart1'
-import p2 from './doorSpritePart2'
-import p3 from './doorSpritePart3'
-import p4 from './doorSpritePart4'
-
-export const doorSpriteUrl = `data:image/webp;base64,${p0}${p1}${p2}${p3}${p4}`
-export const doorSpriteCount = 7
