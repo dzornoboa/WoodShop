27a8c8bb13dd8a85a748ba2f03b2125bd9ff8fd1
Dzornoboa <37811509+dzornoboa@users.noreply.github.com>
2026-09-24 14:42:39 +0000

    Add application routes

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import NailCursor from './components/NailCursor'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => window.scrollTo(0, 0), [pathname])
+  return null
+}
+
+export default function App() {
+  return (
+    <>
+      <NailCursor />
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
